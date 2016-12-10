ed4d379634e6ff415162d04379d5e62d732e3094
Arnout Engelen <arnouten@bzzt.net>
2016-12-10 15:53:21 +0100

    Return a failing promise when starting ensime fails

--- src/lib/ensime-client-startup.ts
+++ src/lib/ensime-client-startup.ts
@@ -5,6 +5,7 @@ import chokidar = require('chokidar');
 import {DotEnsime, ServerStarter} from './types';
 import * as Promise from 'bluebird';
 import {ensureExists} from './file-utils'
+import {ChildProcess} from 'child_process';
 
 const log = loglevel.getLogger('ensime.startup');
 import {createConnection, ServerConnection} from './server-api/server-connection';
@@ -35,8 +36,16 @@ export default function(serverStarter: ServerStarter) {
             resolve(connection);
           });
         } else {
-          let serverPid = undefined
-          
+          function whenFailed(proc: ChildProcess) {
+            return new Promise((resolve, reject) => {
+              // TODO poll instead of checking once
+              // TODO typescript doesn't appear to believe ChildProcess has an `exitCode` field?
+              if (proc.exitCode != 0) {
+                resolve();
+              }
+            });
+          }
+
           function whenAdded(file: string) {
             return new Promise((resolve, reject) => {
               log.debug('starting watching for : '+file)
@@ -53,18 +62,22 @@ export default function(serverStarter: ServerStarter) {
             });
           }
 
-          whenAdded(httpPortFilePath).then( () => {
-            log.debug("got a port file");
-            const httpPort = removeTrailingNewline(fs.readFileSync(httpPortFilePath).toString());
-            const connectionPromise = createConnection(httpPort, generalHandler, serverPid)
-            connectionPromise.then((connection) => {
-              log.debug("got a connection");
-              resolve(connection);
-            });
-          });
-
           log.debug('no server running, start that first…')
-          serverStarter(parsedDotEnsime).then((pid) => serverPid = pid)
+          serverStarter(parsedDotEnsime).then((serverPid) => {
+            whenAdded(httpPortFilePath).then( () => {
+              log.debug("got a port file");
+              const httpPort = removeTrailingNewline(fs.readFileSync(httpPortFilePath).toString());
+              const connectionPromise = createConnection(httpPort, generalHandler, serverPid)
+              connectionPromise.then((connection) => {
+                log.debug("got a connection");
+                resolve(connection);
+              });
+            });
+            whenFailed(serverPid).then( () => {
+              log.debug("Starting ensime failed", serverPid);
+              reject(serverPid);
+            })
+          })
         }
 
       }, (failToCreateCacheDir) => {
